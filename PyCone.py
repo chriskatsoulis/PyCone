c140538a20c30fee0c55bd0a91e38e8e6890fd07
chriskatsoulis <katsoulc@oregonstate.edu>
2023-05-21 23:08:40 -0400

    Third commit

--- PyCone.py
+++ PyCone.py
@@ -1,42 +1,113 @@
 import discord
+from discord.ext import commands
 import openai
+import os
+import asyncio
 
 intents = discord.Intents.default()
 intents.message_content = True
 
-token = 'MTEwODY5MTcxNjA2MTUyODI0Nw.GYj7vK.qmVaEu4O-dKlkA059UduMXxbXZES0JsvyHNe1Q'
-openai.api_key = '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'
+openai.api_key = ''     # insert OpenAI token
 
-client = discord.Client(intents=intents)
-
+client = commands.Bot(command_prefix="=", intents=discord.Intents.all())
 
 @client.event
 async def on_ready():
+    await client.tree.sync()
     print('We have logged in as {0.user}'.format(client))
 
 
-@client.event
-async def on_message(message):
-    username = str(message.author).split('#')[0]
-    user_message = str(message.content)
-    channel = str(message.channel.name)
+class Difficulty:
+    def __init__(self, string):
+        self._string = string
 
-    print(username + " said " + user_message.lower() + " in " + channel)
+    def get_string(self):
+        return self._string
 
-    if message.channel.name == client.user:
-        return
 
-    if message.channel.name == 'development':
-        response = openai.Completion.create(
-                model="text-davinci-003",
+class SelectDifficultyMenu(discord.ui.View):
+
+    options = [
+        discord.SelectOption(label="Easy", value="1", description=None),
+        discord.SelectOption(label="Medium", value="2", description=None),
+        discord.SelectOption(label="Hard", value="3", description=None)
+    ]
+
+    @discord.ui.select(placeholder="Select Difficulty", options=options)
+    async def menu_callback(self, interaction: discord.Interaction, select):
+        select.disabled=True
+        if select.values[0] == "1":
+            difficulty = Difficulty("Easy")
+            await interaction.response.send_message(content="Select topic!", view=SelectTopicMenu())
+
+        elif select.values[0] == "2":
+            difficulty = Difficulty("Medium")
+            await interaction.response.send_message(content="Select topic!", view=SelectTopicMenu())
+
+        elif select.values[0] == "3":
+            difficulty = Difficulty("Hard")
+            await interaction.response.send_message(content="Select topic!", view=SelectTopicMenu())
+
+
+class SelectTopicMenu(discord.ui.View):
+
+    options = [
+        discord.SelectOption(label="Binary Search", value="1", description=None),
+        discord.SelectOption(label="Combinatorics", value="2", description=None),
+        discord.SelectOption(label="Linked List", value="3", description=None),
+        discord.SelectOption(label="Recursion", value="4", description=None),
+        discord.SelectOption(label="Sorting", value="5", description=None)
+    ]
+
+    @discord.ui.select(placeholder="Select Topic", options=options)
+    async def menu_callback(self, interaction: discord.Interaction, select):
+        select.disabled=True
+        if select.values[0] == "1":
+            user_message = """
+            Please generate a Python problem of easy
+            difficulty that involves binary search. Do not reveal the answer or in any 
+            way show any sort of Python code. I want to try the problem on my own, so 
+            don't spoil it!
+            """
+            channel = str(user_message.channel.name)
+            username = str(user_message.author).split('#')[0]
+            response = openai.Completion.create(
+                engine='text-davinci-003',
                 prompt=user_message,
-                max_tokens=3000,
-                temperature=0.7
+                max_tokens=100,
+                temperature=0.7,
+                n=1,
+                stop=None
             )
+            chatgpt_response = response.choices[0].text.strip()
+            print(chatgpt_response)
+            await interaction.response.send_message(content=chatgpt_response)
 
-        output = response["choices"][0]["text"]
+        elif select.values[0] == "2":
+            await interaction.response.send_message(content=None)
 
-        print(output)
-        await message.channel.send(output)
+        elif select.values[0] == "3":
+            await interaction.response.send_message(content=None)
 
-client.run(token)
+        elif select.values[0] == "4":
+            await interaction.response.send_message(content=None)
+
+        elif select.values[0] == "5":
+            await interaction.response.send_message(content=None)
+
+
+@client.tree.command(name="practicequestion", description="Generate a Python practice question from ChatGPT!")
+async def PracticeQueston(interaction: discord.Interaction):
+    await interaction.response.send_message(content="Select difficulty!", view=SelectDifficultyMenu())
+
+async def load():
+    for filename in os.listdir("./cogs"):
+        if filename.endswith(".py"):
+            await client.load_extension(f"cogs.{filename[:-3]}")
+
+async def main():
+    async with client:
+        await load()
+        await client.start(')       # insert Discord token
+
+asyncio.run(main())
